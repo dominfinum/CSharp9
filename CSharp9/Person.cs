ef731b650164b713caa1b79f7ef1bf89a7289214
Dominik Blažić <dominik.blazic@gmail.com>
2021-06-14 23:10:38 +0200

    Records

---
+++ CSharp9/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp9
+{
+    //public record PersonRec(string FirstName, string LastName);
+    public record PersonRec(string FirstName, string LastName)
+    {
+        public string FirstName { get; init; } = FirstName;
+        public string LastName { get; init; } = LastName;
+
+        public string HelloWorld()
+        {
+            return "Hello world!";
+        }
+    }
+
+    public record Developer(string FavoriteLanguage, string FirstName, string LastName) : PersonRec(FirstName, LastName);
+
+
+
+    public class PersonClass
+    {
+        public PersonClass(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        
+
+    }
+}
---
+++ CSharp9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSharp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonRec personRec = new("Marko", "Nikolic");
+            PersonRec personRec1 = new("Marko", "Nikolic");
+            PersonRec personRec2 = new("Marko", "Dosen");
+
+            PersonClass personCl = new("Dona", "Pejnovic");
+            PersonClass personCl1 = new("Dona", "Pejnovic");
+
+            //Console.WriteLine("Record Type:");
+            //Console.WriteLine($"To string: { personRec }");
+            //Console.WriteLine($"Equals: { Equals(personRec, personRec1) }");
+            //Console.WriteLine($"Reference Equals: { ReferenceEquals(personRec, personRec1) }");
+            //Console.WriteLine($"==: { personRec == personRec1 }");
+            //Console.WriteLine($"Hash code: { personRec.GetHashCode() }");
+            //Console.WriteLine($"Hash code: { personRec1.GetHashCode() }");
+            //Console.WriteLine($"Hash code: { personRec2.GetHashCode() }");
+
+            //Console.WriteLine("****************");
+            //Console.WriteLine("Class Type:");
+            //Console.WriteLine($"To string: { personCl }");
+            //Console.WriteLine($"Equals: { Equals(personCl, personCl1) }");
+            //Console.WriteLine($"Reference Equals: { ReferenceEquals(personCl, personCl1) }");
+            //Console.WriteLine($"==: { personCl == personCl1 }");
+            //Console.WriteLine($"Hash code: { personCl.GetHashCode() }");
+            //Console.WriteLine($"Hash code: { personCl1.GetHashCode() }");
+
+            PersonRec personRec4 = personRec with
+            {
+                LastName = "Dosen"
+            };
+            
+            //Console.WriteLine($"To string: { personRec4 }");
+
+            var (fn, ln) = personRec4;
+            Console.WriteLine($"First name: { fn } Last name: { ln }");
+
+
+
+
+        }
+    }
+
+    
+}
